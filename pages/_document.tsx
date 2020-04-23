dd97c6055148b76b7cff41dae4c39bb25492ac49
Ljubomir Ignov <ljubomir_ignov@hotmail.com>
2020-04-23 17:39:22 +0200

    Initial setup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,20 @@
-import Document, { DocumentContext } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, {
+  DocumentContext, Main, NextScript,
+} from 'next/document';
+import Head from 'next/head';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
-        })
+      ctx.renderPage = () => originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
@@ -21,9 +23,25 @@ export default class MyDocument extends Document {
             {sheet.getStyleElement()}
           </>
         ),
-      }
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <title>Modern Web App</title>
+          {/* Step 5: Output the styles in the head  */}
+          {this.props.styles}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
   }
 }
